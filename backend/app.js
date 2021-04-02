1ffe3fe6b1ed4a5cd143d06151cba69df0c4aabe
Mira <mirajane.aye@hotmail.fr>
2021-04-02 14:26:24 +0200

    Adding files

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('Request received');
+    next();
+})
+
+app.use(((req, res, next) => {
+    res.status(201);
+    next();
+}))
+
+app.use((req, res, next) => {
+    res.json({message: 'Your request has been received!'});
+});
+
+module.exports = app;
